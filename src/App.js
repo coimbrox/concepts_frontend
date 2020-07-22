5d180cef2c7cd6340868c088289e6cec503d1e04
coimbrox <gabao.coimbra@gmail.com>
2020-07-22 16:46:01 -0300

    Aplicando Conceitos de Componentização

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import Header from './components/Header';
+
+function App() {
+  return (
+    // conceito de fragments
+    <>
+      <Header />
+      <Header />
+    </>
+  );
+}
+
+
+export default App;
